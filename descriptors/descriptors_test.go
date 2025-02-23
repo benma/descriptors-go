945c75b0b2f1acb4316dcf12ce4208c2d750c529
Oliver Gugger <gugger@gmail.com>
2025-02-23 10:50:27 +0100

    descriptors: turn tests into test vectors
    Makes it easy to add more test cases by extracting the test case values
    into a JSON based test vector file.

--- descriptors/descriptors_test.go
+++ descriptors/descriptors_test.go
@@ -1,125 +1,116 @@
 package descriptors
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestDescriptorSingle(t *testing.T) {
-	descriptorStr := "wpkh(xpub6BzikmgQmvoYG3ShFhXU1LFKaUeU832dHoYL6ka9JpCqKXr7PTHQHaoSMbGU36CZNcoryVPsFBjt9aYyCQHtYi6BQTo6VfRv9xVRuSNNteB/*)"
-	checksum := "#97qc8vss"
-	descriptor, err := NewDescriptor(descriptorStr)
-	require.NoError(t, err)
-	defer descriptor.Close()
-	require.Equal(t, 1, descriptor.MultipathLen())
-	require.Equal(t, descriptorStr+checksum, descriptor.String())
+const (
+	testdataDir           = "testdata"
+	derivationTestVectors = "derivation.json"
+)
 
-	addr, err := descriptor.AddressAt(NetworkMainnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qaz3jjsgpe29v5yzrvd58hsjgx5a9msujsgmyte", addr)
-
-	_, err = descriptor.AddressAt(NetworkMainnet, 1, 0)
-	require.Error(t, err)
+func parseNetwork(t *testing.T, network string) Network {
+	switch network {
+	case "mainnet":
+		return NetworkMainnet
+	case "testnet":
+		return NetworkTestnet
+	case "regtest":
+		return NetworkRegtest
+	default:
+		require.FailNow(t, "unknown network: %s", network)
+		return 0
+	}
 }
 
-func TestDescriptorMultipath(t *testing.T) {
-	descriptorStr := "wpkh(xpub6BzikmgQmvoYG3ShFhXU1LFKaUeU832dHoYL6ka9JpCqKXr7PTHQHaoSMbGU36CZNcoryVPsFBjt9aYyCQHtYi6BQTo6VfRv9xVRuSNNteB/<0;1>/*)"
-	checksum := "#w3nj0e22"
-	descriptor, err := NewDescriptor(descriptorStr)
+func TestDerivationVectors(t *testing.T) {
+	vectors, err := readDerivationTestVectors()
 	require.NoError(t, err)
-	defer descriptor.Close()
 
-	require.Equal(t, 2, descriptor.MultipathLen())
-	require.Equal(t, descriptorStr+checksum, descriptor.String())
+	for _, vector := range vectors {
+		t.Run(vector.Name, func(t *testing.T) {
+			descriptor, err := NewDescriptor(vector.Descriptor)
 
-	// First receive address.
-	addr, err := descriptor.AddressAt(NetworkMainnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qrdu5f9wfw3ppxmejnqe6rexd7fqkujnpzar5es", addr)
+			if vector.ExpectErr != "" {
+				require.ErrorContains(t, err, vector.ExpectErr)
+				return
+			}
 
-	// 11th receive address.
-	addr, err = descriptor.AddressAt(NetworkMainnet, 0, 10)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qsmp7eq2jt00llmsqmtatwavkjkdmrrz9klgd5g", addr)
+			require.NoError(t, err)
+			defer descriptor.Close()
 
-	// First change address.
-	addr, err = descriptor.AddressAt(NetworkMainnet, 1, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qlh4ln6vjdkz89um8gtzfmpf6wnt56lwy4u466g", addr)
+			require.EqualValues(
+				t, vector.NumMultipath,
+				descriptor.MultipathLen(),
+			)
 
-	// 11th change address.
-	addr, err = descriptor.AddressAt(NetworkMainnet, 1, 10)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qpnmw93dscgt9m6shjq0v83z02tcljf8yfc9j7l", addr)
+			expectedDescriptor := vector.Descriptor
+			if !vector.HasChecksum {
+				expectedDescriptor += vector.Checksum
+			}
+			require.Equal(
+				t, expectedDescriptor, descriptor.String(),
+			)
+
+			for _, addr := range vector.Addresses {
+				network := parseNetwork(t, addr.Network)
+
+				parsedAddr, err := descriptor.AddressAt(
+					network, addr.MultipathIndex,
+					addr.DerivationIndex,
+				)
+
+				if addr.ExpectErr != "" {
+					require.ErrorContains(
+						t, err, addr.ExpectErr,
+					)
+					continue
+				}
+
+				require.NoError(t, err)
+				require.Equal(t, addr.Address, parsedAddr)
+			}
+		})
+	}
 }
 
-func TestNetworksWpkh(t *testing.T) {
-	descriptorStr := "wpkh(xpub6BzikmgQmvoYG3ShFhXU1LFKaUeU832dHoYL6ka9JpCqKXr7PTHQHaoSMbGU36CZNcoryVPsFBjt9aYyCQHtYi6BQTo6VfRv9xVRuSNNteB/<0;1>/*)"
-	checksum := "#w3nj0e22"
-	descriptor, err := NewDescriptor(descriptorStr)
-	require.NoError(t, err)
-	defer descriptor.Close()
-
-	require.Equal(t, descriptorStr+checksum, descriptor.String())
-
-	addr, err := descriptor.AddressAt(NetworkMainnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1qrdu5f9wfw3ppxmejnqe6rexd7fqkujnpzar5es", addr)
-
-	addr, err = descriptor.AddressAt(NetworkTestnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "tb1qrdu5f9wfw3ppxmejnqe6rexd7fqkujnpgmc8zr", addr)
-
-	addr, err = descriptor.AddressAt(NetworkRegtest, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bcrt1qrdu5f9wfw3ppxmejnqe6rexd7fqkujnp2jp242", addr)
+type testAddress struct {
+	Network         string
+	MultipathIndex  uint32
+	DerivationIndex uint32
+	Address         string
+	ExpectErr       string
 }
 
-func TestNetworksTr(t *testing.T) {
-	descriptorStr := "tr(xpub6BzikmgQmvoYG3ShFhXU1LFKaUeU832dHoYL6ka9JpCqKXr7PTHQHaoSMbGU36CZNcoryVPsFBjt9aYyCQHtYi6BQTo6VfRv9xVRuSNNteB/<0;1>/*)"
-	checksum := "#rq20sev9"
-	descriptor, err := NewDescriptor(descriptorStr)
-	require.NoError(t, err)
-	defer descriptor.Close()
-
-	require.Equal(t, descriptorStr+checksum, descriptor.String())
-
-	addr, err := descriptor.AddressAt(NetworkMainnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1puc00mgvj7hh25y8uhpux7gudjp9a986awgtemtxdfx6kcv4dkl5qt275jk", addr)
-
-	addr, err = descriptor.AddressAt(NetworkTestnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "tb1puc00mgvj7hh25y8uhpux7gudjp9a986awgtemtxdfx6kcv4dkl5quzgmge", addr)
-
-	addr, err = descriptor.AddressAt(NetworkRegtest, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bcrt1puc00mgvj7hh25y8uhpux7gudjp9a986awgtemtxdfx6kcv4dkl5q3mzaar", addr)
+type derivationTestVector struct {
+	Name         string
+	Descriptor   string
+	HasChecksum  bool
+	Checksum     string
+	NumMultipath uint32
+	ExpectErr    string
+	Addresses    []*testAddress
 }
 
-func TestInvalidChecksum(t *testing.T) {
-	_, err := NewDescriptor("tr([e81a5744/48'/0'/0'/2']xpub6Duv8Gj9gZeA3sUo5nUMPEv6FZ81GHn3feyaUej5KqcjPKsYLww4xBX4MmYZUPX5NqzaVJWYdYZwGLECtgQruG4FkZMh566RkfUT2pbzsEg/<0;1>/*,and_v(v:pk([3c157b79/48'/0'/0'/2']xpub6DdSN9RNZi3eDjhZWA8PJ5mSuWgfmPdBduXWzSP91Y3GxKWNwkjyc5mF9FcpTFymUh9C4Bar45b6rWv6Y5kSbi9yJDjuJUDzQSWUh3ijzXP/<0;1>/*),older(65535)))#lg9nqqha")
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "Invalid checksum")
-}
+// readDerivationTestVectors reads the derivation test vectors from the test
+// vector file.
+func readDerivationTestVectors() ([]*derivationTestVector, error) {
+	// Open the test vector file.
+	file, err := os.Open(filepath.Join(testdataDir, derivationTestVectors))
+	if err != nil {
+		return nil, err
+	}
 
-func TestTapminiscript(t *testing.T) {
-	descriptorStr := "tr([e81a5744/48'/0'/0'/2']xpub6Duv8Gj9gZeA3sUo5nUMPEv6FZ81GHn3feyaUej5KqcjPKsYLww4xBX4MmYZUPX5NqzaVJWYdYZwGLECtgQruG4FkZMh566RkfUT2pbzsEg/<0;1>/*,and_v(v:pk([3c157b79/48'/0'/0'/2']xpub6DdSN9RNZi3eDjhZWA8PJ5mSuWgfmPdBduXWzSP91Y3GxKWNwkjyc5mF9FcpTFymUh9C4Bar45b6rWv6Y5kSbi9yJDjuJUDzQSWUh3ijzXP/<0;1>/*),older(65535)))#lg9nqqhr"
-	descriptor, err := NewDescriptor(descriptorStr)
-	require.NoError(t, err)
-	defer descriptor.Close()
+	// Decode the test vectors.
+	var testVectors []*derivationTestVector
+	if err := json.NewDecoder(file).Decode(&testVectors); err != nil {
+		return nil, err
+	}
 
-	require.Equal(t, 2, descriptor.MultipathLen())
-	require.Equal(t, descriptorStr, descriptor.String())
-
-	// First receive address.
-	addr, err := descriptor.AddressAt(NetworkMainnet, 0, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1pfujezshxnw0jw2m9fn6hz0xr3ljjpjd30gtpqyymgxqquk8rr95qk986dz", addr)
-
-	// First change address.
-	addr, err = descriptor.AddressAt(NetworkMainnet, 1, 0)
-	require.NoError(t, err)
-	require.Equal(t, "bc1ptd9ngyt09kxtt8mkf832vhxdrn0xhccwp9jv6mz6vv4wqfc8dxjql2y0fn", addr)
+	return testVectors, nil
 }
